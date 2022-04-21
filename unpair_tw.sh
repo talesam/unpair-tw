4e0cdf1d757f192f7e863cae6f65a7ff5b088bc5
Tales A. Mendonça <talesam@gmail.com>
2022-04-21 19:02:06 -0300

    Correções na identação

--- unpair_tw.sh
+++ unpair_tw.sh
@@ -11,20 +11,20 @@ VER="v0.0.3"
 # Tabela de cores: https://misc.flogisoft.com/_media/bash/colors_format/256_colors_fg.png
 
 # Cores degrade
-RED001='\e[38;5;1m'         # Vermelho 1
-RED009='\e[38;5;9m'         # Vermelho 9
-CYA122='\e[38;5;122m'		# Ciano 122
-CYA044='\e[38;5;44m'		# Ciano 44
-ROX063='\e[38;5;63m'		# Roxo 63
-ROX027='\e[38;5;27m'		# Roxo 27
-GRE046='\e[38;5;46m'		# Verde 46
-GRY247='\e[38;5;247m'		# Cinza 247
-LAR208='\e[38;5;208m'		# Laranja 208
-LAR214='\e[38;5;214m'		# Laranja 214
-AMA226='\e[38;5;226m'		# Amarelo 226
-BLU039='\e[38;5;44m'		# Azul 39
-MAR094='\e[38;5;94m'		# Marrom 94
-MAR136='\e[38;5;136m'		# Marrom 136
+RED001='\e[38;5;1m'		# Vermelho 1
+RED009='\e[38;5;9m'		# Vermelho 9
+CYA122='\e[38;5;122m'	# Ciano 122
+CYA044='\e[38;5;44m'	# Ciano 44
+ROX063='\e[38;5;63m'	# Roxo 63
+ROX027='\e[38;5;27m'	# Roxo 27
+GRE046='\e[38;5;46m'	# Verde 46
+GRY247='\e[38;5;247m'	# Cinza 247
+LAR208='\e[38;5;208m'	# Laranja 208
+LAR214='\e[38;5;214m'	# Laranja 214
+AMA226='\e[38;5;226m'	# Amarelo 226
+BLU039='\e[38;5;44m'	# Azul 39
+MAR094='\e[38;5;94m'	# Marrom 94
+MAR136='\e[38;5;136m'	# Marrom 136
 
 # Cores chapadas
 CIN='\e[30;1m'			# Cinza
@@ -36,7 +36,7 @@ ROS='\e[35;1m'			# Rosa
 CYA='\e[36;1m'			# Ciano
 NEG='\e[37;1m'			# Negrito
 CUI='\e[40;31;5m'		# Vermelho pisacando, aviso!
-STD='\e[m'              # Fechamento de cor
+STD='\e[m'				# Fechamento de cor
 
 # --- Início Funções --- 
 
@@ -49,33 +49,33 @@ separacao(){
 
 # Função pause
 pause(){
-   read -p "$*"
+	read -p "$*"
 }
 
 # Verifica se está usando Termux
 termux(){
 	clear
-    separacao
+	separacao
 	echo -e " ${NEG}Bem vindo(a) ao script${STD} ${ROS}Unpair Ticwatch${STD} - ${CIN}${VER}${STD}"
 	echo -e " ${NEG}Modelos compatíveis: PRO 3 ULTRA.${STD}"
 	separacao
-    echo -e " ${CYA}Observação:${STD} Se essa é a primeira vez que utiliza"
-    echo -e " este script, deverá ativar o modo DEPURAÇÂO no"
-    echo -e " seu relógio. Vá em ${YEL}Configurações${STD}, ${YEL}Sistema${STD}, ${YEL}Sobre${STD} e"
-    echo -e " toque em ${YEL}Número da versão${STD} 7 vezes, rapidamente,"
-    echo -e " para desbloquear as opções de desenvolvedor."
-    echo ""
-    echo -e " Em ${YEL}configurações${STD} terá ${YEL}Opções do desenvolvedor${STD}"
-    echo -e " abaixo de ${YEL}Sistema${STD}. Ative as opções ${YEL}Depuração USB${STD}"
-    echo -e " e ${YEL}Depurar por Wifi${STD}. O relógio pode exibir inicialmente"
-    echo -e " uma mensagem dizendo ${YEL}Indisponível${STD} que em breve será"
-    echo -e " substituída por uma sequência de caracteres incluindo o"
-    echo -e " endereço IP. Isso significa que o ADB sobre Wi-Fi"
-    echo -e " foi ativado. Anote o endereço IP exibido aqui."
-    echo -e " Será algo como:"
-    echo ""
-    echo -e " ${ROS}192.168.68.119${STD}"
-    separacao
+	echo -e " ${CYA}Observação:${STD} Se essa é a primeira vez que utiliza"
+	echo -e " este script, deverá ativar o modo DEPURAÇÂO no"
+	echo -e " seu relógio. Vá em ${YEL}Configurações${STD}, ${YEL}Sistema${STD}, ${YEL}Sobre${STD} e"
+	echo -e " toque em ${YEL}Número da versão${STD} 7 vezes, rapidamente,"
+	echo -e " para desbloquear as opções de desenvolvedor."
+	echo ""
+	echo -e " Em ${YEL}configurações${STD} terá ${YEL}Opções do desenvolvedor${STD}"
+	echo -e " abaixo de ${YEL}Sistema${STD}. Ative as opções ${YEL}Depuração USB${STD}"
+	echo -e " e ${YEL}Depurar por Wifi${STD}. O relógio pode exibir inicialmente"
+	echo -e " uma mensagem dizendo ${YEL}Indisponível${STD} que em breve será"
+	echo -e " substituída por uma sequência de caracteres incluindo o"
+	echo -e " endereço IP. Isso significa que o ADB sobre Wi-Fi"
+	echo -e " foi ativado. Anote o endereço IP exibido aqui."
+	echo -e " Será algo como:"
+	echo ""
+	echo -e " ${ROS}192.168.68.119${STD}"
+	separacao
 	echo "" 
 	pause "Tecle [Enter] para continuar..."
 	clear
@@ -88,7 +88,7 @@ termux(){
 	else
 		echo -e " ${BLU}*${STD} ${NEG}Baixando as dependências para utilizar o script${SDT}"
 		echo -e " ${BLU}*${STD} ${NEG}no Termux, aguarde a conclusão...${SDT}" && sleep 2
-        pkg update -y -o Dpkg::Options::=--force-confold
+		pkg update -y -o Dpkg::Options::=--force-confold
 		pkg install -y android-tools && clear
 		if [ "$?" -eq "0" ]; then
 			echo ""
@@ -105,7 +105,7 @@ termux(){
 # Conexão do relógio
 conectar_relogio(){
 	clear
-    echo ""
+	echo ""
 	echo " Digite o endereço IP do seu relógio que encontra-se no"
 	echo -e " caminho abaixo e tecle ${NEG}[Enter]${STD} para continuar:"
 	echo ""
@@ -127,8 +127,8 @@ conectar_relogio(){
 			until adb shell pm list packages -e >/dev/null; do
 				echo ""
 				echo -e " ${NEG}Autorize a conexão no seu relógio${STD}"
-                echo -e " Aparecerá no relógio: ${BLU}Depuração USB?${STD}"
-                echo -e " Toque em ${GRE}OK${STD} ou em${STD}"
+				echo -e " Aparecerá no relógio: ${BLU}Depuração USB?${STD}"
+				echo -e " Toque em ${GRE}OK${STD} ou em${STD}"
 				echo -e " ${GRE}Sempre permitir a partir deste computador${STD}."
 				echo ""
 				pause " Tecle [Enter] para continuar..." ;
@@ -157,47 +157,47 @@ conectar_relogio(){
 # Desemparelhar
 desemparelhar(){
 	echo ""
-    echo -e " ${GRE046}Reiniciando o relógio...${STD}" && sleep 2
-    # Limpando as configurações e reiniciando o relógio
-    if [ "$(adb connect $IP | cut -f1,2 -d" ")" = "already connected" ]; then
-        adb shell "pm clear com.google.android.gms && reboot" >/dev/null
-        # Se a execução for bem sussedida, então...
-        if [ "$?" -eq "0" ]; then
-            echo ""
-            echo -e " ${GRE046}Limpando as configurações...${STD}" && sleep 2
-            # Verifica se o relógio já reiniciou e conectou via adb
-            echo ""
-            echo -e " ${CYA044}Aguardando o relógio se conectar...😴${STD}"
-            until $(ping -c 1 $IP >/dev/null 2>&1); do
-                # Aguardando relógio se conectar
-                echo -ne "."
-                sleep 1
-            done
+	echo -e " ${GRE046}Reiniciando o relógio...${STD}" && sleep 2
+	# Limpando as configurações e reiniciando o relógio
+	if [ "$(adb connect $IP | cut -f1,2 -d" ")" = "already connected" ]; then
+		adb shell "pm clear com.google.android.gms && reboot" >/dev/null
+		# Se a execução for bem sussedida, então...
+		if [ "$?" -eq "0" ]; then
 			echo ""
-            echo -e " Vá em ${AMA226}Configurações${STD}, ${AMA226}Opções do desenvolvedor${STD},"
-	        echo -e " desative e ative a ${AMA226}Depuração USB${STD}"
-            echo -e " e aguarde pegar o IP em ${AMA226}Depurar por Wi-Fi${STD}"
-            pause " Quando aparece, tecle [Enter] para continuar..."
-            # Conecta ao relógio após reiniciar
-            adb connect $IP >/dev/null
-            if [ "$(adb connect $IP | cut -f1,2 -d" ")" = "already connected" ]; then
-                echo ""
-                echo -e " ${GRE}*${STD} ${NEG}Relógio conectado com sucesso!!${STD}" && sleep 2
-            else
-                echo -e " ${RED}*${STD} ${NEG}Erro! Falha ao desparear.${STD}"
-			    pause " Tecle [Enter] para tentar novamente..."
-            fi
-            # Desparear o dispositivo bluetooth do relógio
-            adb shell "am start -a android.bluetooth.adapter.action.REQUEST_DISCOVERABLE" >/dev/null
-            if [ "$?" -eq "0" ]; then
-                echo -e " ${ROS}Vá para o seu smartphone configure o relógio novamente.${STD}"
+			echo -e " ${GRE046}Limpando as configurações...${STD}" && sleep 2
+			# Verifica se o relógio já reiniciou e conectou via adb
+			echo ""
+			echo -e " ${CYA044}Aguardando o relógio se conectar...😴${STD}"
+			until $(ping -c 1 $IP >/dev/null 2>&1); do
+				# Aguardando relógio se conectar
+				echo -ne "."
+				sleep 1
+			done
+			echo ""
+			echo -e " Vá em ${AMA226}Configurações${STD}, ${AMA226}Opções do desenvolvedor${STD},"
+			echo -e " desative e ative a ${AMA226}Depuração USB${STD}"
+			echo -e " e aguarde pegar o IP em ${AMA226}Depurar por Wi-Fi${STD}"
+			pause " Quando aparece, tecle [Enter] para continuar..."
+			# Conecta ao relógio após reiniciar
+			adb connect $IP >/dev/null
+			if [ "$(adb connect $IP | cut -f1,2 -d" ")" = "already connected" ]; then
+				echo ""
+				echo -e " ${GRE}*${STD} ${NEG}Relógio conectado com sucesso!!${STD}" && sleep 2
+			else
+				echo -e " ${RED}*${STD} ${NEG}Erro! Falha ao desparear.${STD}"
+				pause " Tecle [Enter] para tentar novamente..."
+			fi
+			# Desparear o dispositivo bluetooth do relógio
+			adb shell "am start -a android.bluetooth.adapter.action.REQUEST_DISCOVERABLE" >/dev/null
+			if [ "$?" -eq "0" ]; then
+				echo -e " ${ROS}Vá para o seu smartphone configure o relógio novamente.${STD}"
 				exit 0
-            else
-                echo -e " ${RED}*${STD} ${NEG}Erro! Falha ao desparear.${STD}"
-			    pause " Tecle [Enter] para tentar novamente..." ; desemparelhar
-            fi
-        fi
-    fi
+			else
+				echo -e " ${RED}*${STD} ${NEG}Erro! Falha ao desparear.${STD}"
+				pause " Tecle [Enter] para tentar novamente..." ; desemparelhar
+			fi
+		fi
+	fi
 }
 # Chama o script principal
 termux
