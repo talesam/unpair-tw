47830900bb55137db49eeaf5f6d070852ec087f9
Tales A. Mendonça <talesam@gmail.com>
2022-04-22 22:54:40 -0300

    Ajuste na checagem por dependencia

--- unpair_tw.sh
+++ unpair_tw.sh
@@ -65,7 +65,7 @@ termux(){
 	clear
 	echo ""
 	echo -e " ${ROX063}Verificando dependências, aguarde...${STD}" && sleep 2
-	if [ -f "/data/data/com.termux/files/usr/bin/adb.bin" ] || [ -f "/usr/bin/adb" ]; then
+	if [ "ls /data/data/com.termux/files/usr/bin/adb.bin > /dev/null" ] || [ "ls /usr/bin/adb > /dev/null" ]; then
 		echo -e " ${GRE046}Dependencias encontradas, conecte-se ao relógio.${STD}"
 		echo ""
 		pause " Tecle [Enter] para continuar..." ; conectar_relogio
